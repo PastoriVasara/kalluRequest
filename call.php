b0d018b6ca421b9622b7099078838a264bbf941e
Jussi Rantala <jussi.p.rantala@tuni.fi>
2019-05-22 20:12:43 +0300

    added the call file

---
+++ call.php
@@ -0,0 +1,50 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
+include('../connectSQL.php');
+
+if (isset($_POST['unit'])) {
+    $unit = isset($_POST['unit']) ? $_POST['unit'] : null;
+    $condition = isset($_POST['condition']) ? $_POST['condition'] : null;
+    $fullQuery = 'SELECT  linkedcourse.ID AS Parent, parent.CODE as Parent_Code, parent.NAME AS Parent_Course, linkedcourse.LINKEDID AS Child, child.CODE AS Child_Code, child.$
+                  FROM linkedcourse JOIN course_shrinked parent ON linkedcourse.ID = parent.ID JOIN course_shrinked child ON linkedcourse.LINKEDID = child.ID
+                  WHERE parent.UNIT = "'.$unit.'" OR child.UNIT = "'.$unit.'" ORDER BY Parent ASC';
+
+    $IDsFROM = array();
+    $childrenIDsFROM = array();
+    $courseNamesFROM = array();
+
+    $IDsTO = array();
+    $childrenIDsTO = array();
+    $courseNamesTO = array();
+    $test = mysqli_query($conn,$fullQuery);
+    if ($returnValue = mysqli_query($conn, $fullQuery)) {
+        if (mysqli_num_rows($returnValue) > 0) {
+            while ($row = mysqli_fetch_array($returnValue)) {
+                $IDsFROM[] = $row[0];
+                $childrenIDsFROM[] = preg_replace('/[^(\x20-\x7F)]*/', '', $row[1]);
+                $courseNamesFROM[] =  utf8_encode($row[2]);
+                $IDsTO[] = $row[3];
+                $childrenIDsTO[] =preg_replace('/[^(\x20-\x7F)]*/', '', $row[4]);
+                $courseNamesTO[] =  utf8_encode($row[5]);
+            }
+       }
+   }
+ echo json_encode(array($IDsFROM,$childrenIDsFROM, $courseNamesFROM, $IDsTO , $childrenIDsTO , $courseNamesTO));
+
+} elseif (isset($_POST['courses'])) {
+    $fullQuery = 'SELECT NAME, CODE FROM course';
+    $courseName = array();
+    $courseCode = array();
+    if ($returnValue = mysqli_query($conn, $fullQuery)) {
+        if (mysqli_num_rows($returnValue) > 0) {
+            while ($row = mysqli_fetch_array($returnValue)) {
+                $courseName[] =  utf8_encode($row[0]);
+                $courseCode[] = preg_replace('/[^(\x20-\x7F)]*/', '', $row[1]);
+            }
+        }
+    }
+    echo json_encode(array($courseName,$courseCode));
+}
+?>
+
